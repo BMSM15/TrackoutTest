5251aeb4d97dfff1202afd2cf567831b04008a02
Bruno Martins <brunomiguelmartins1522@gmail.com>
2024-03-22 22:45:41 +0000

    Page introduction almost done

--- client/src/pages/Introduction/Introduction.jsx
+++ client/src/pages/Introduction/Introduction.jsx
@@ -12,9 +12,13 @@ const Introduction = ({ user, logout, sidebar, setSidebar }) => {
         <span>
           PRISMA <p className={styles.p1}>PROTOCOL</p>{" "}
         </span>
-        <p>BETA</p>
+        <a>BETA</a>
       </div>
-      <div className={styles.intro_container}>
+      <div
+        className={
+          sidebar ? styles.intro_container : styles.intro_containeractive
+        }
+      >
         <h1>
           All Music Royalties <br></br> in one Ecosystem.
         </h1>
@@ -22,7 +26,10 @@ const Introduction = ({ user, logout, sidebar, setSidebar }) => {
           There are multiple music royalties and you might be missing some of
           them.<br></br> We make sure you don’t.
         </p>
-        <button className={styles.intro_button}> Waitlist is now open </button>
+        <button className={styles.intro_button} onClick={showSidebar}>
+          {" "}
+          Waitlist is now open{" "}
+        </button>
         <div className={styles.intro_cards}>
           <div className={styles.card}>
             <p>For Independent Artists</p>
@@ -31,6 +38,68 @@ const Introduction = ({ user, logout, sidebar, setSidebar }) => {
           <div className={styles.card}>
             <p>For Independent Songwriters</p>
             <span>Who own their Copyright</span>
+          </div>
+        </div>
+      </div>
+      <div
+        className={
+          sidebar ? styles.form_containeractive : styles.form_container
+        }
+      >
+        <div className={sidebar ? styles.formactive : styles.form}>
+          <p>Join the Waitlist by filling the following information:</p>
+          <div className={styles.input_text_section}>
+            <div className={styles.input_text}>
+              <label>
+                What’s your name?<span>*</span>
+              </label>
+              <input type="text" placeholder="Write your real or artist name" />
+            </div>
+            <div className={styles.input_text}>
+              <label>
+                What’s your e-mail?<span>*</span>
+              </label>
+              <input type="text" placeholder="Write your e-mail address" />
+            </div>
+            <div className={styles.input_text}>
+              <label>
+                Link<span>*</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Paste a Link (Spotify, Instagram or Website)"
+              />
+            </div>
+            <div className={styles.input_checkboxgroup}>
+              <p>What type of Royalties do you collect?</p>
+              <div className={styles.input_checkboxdivision}>
+                <div className={styles.input_checkbox_minigroup}>
+                  <div className={styles.input_checkbox}>
+                    <input type="checkbox" />
+                    <label>Master Rights</label>
+                  </div>
+                  <div className={styles.input_checkbox}>
+                    <input type="checkbox" />
+                    <label>Mechanical Rights</label>
+                  </div>
+                </div>
+                <div className={styles.input_checkbox_minigroup}>
+                  <div className={styles.input_checkbox}>
+                    <input type="checkbox" />
+                    <label>Performance Rights</label>
+                  </div>
+                  <div className={styles.input_checkbox}>
+                    <input type="checkbox" />
+                    <label>Neighboring Rights</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.button}>
+              <button onClick={showSidebar} className={styles.submit_button}>
+                Submit
+              </button>
+            </div>
           </div>
         </div>
       </div>
